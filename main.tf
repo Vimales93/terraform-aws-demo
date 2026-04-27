10834a2b2f7636545731fdf5c82d718dbbb14626
Vimaleswaran <vimales93@gmail.com>
2026-04-27 13:43:09 +0530

    Add test S3 object

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "demo" {
   bucket = "vimal-demo-bucket-12345"
 }
+
+resource "aws_s3_object" "test_file" {
+  bucket = "vimal-demo-bucket-12345"
+  key    = "test-file.txt"
+  content = "This is a test file created by Terraform via TeamCity pipeline"
+}
